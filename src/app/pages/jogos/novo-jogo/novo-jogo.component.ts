c4fdee1a519c3a3596123c79b3807226ab6afc1a
Expedito <expeditopaivaneto@gmail.com>
2024-09-17 13:21:10 -0300

    Criando pagina de novo-jogo

---
+++ src/app/pages/jogos/novo-jogo/novo-jogo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-jogo',
+  templateUrl: './novo-jogo.component.html',
+  styleUrl: './novo-jogo.component.scss'
+})
+export class NovoJogoComponent {
+
+}
