a433cfb0bfb117b1bba968a529698880c90b2301
Expedito <expeditopaivaneto@gmail.com>
2024-09-17 13:19:29 -0300

    Criando pagina de lista-jogos

---
+++ src/app/pages/jogos/lista-jogos/lista-jogos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-jogos',
+  templateUrl: './lista-jogos.component.html',
+  styleUrl: './lista-jogos.component.scss'
+})
+export class ListaJogosComponent {
+
+}
