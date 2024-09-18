c4f29083d0eb1d3e234cde46766beb822cb3a2b1
Expedito <expeditopaivaneto@gmail.com>
2024-09-17 23:48:03 -0300

    criando rota de visualizar jogo e pegar pelo id

--- src/app/pages/jogos/jogos-routing.module.ts
+++ src/app/pages/jogos/jogos-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 //Pages
 import { ListaJogosComponent } from './lista-jogos/lista-jogos.component';
 import { NovoJogoComponent } from './novo-jogo/novo-jogo.component';
+import { VisualizarJogoComponent } from './visualizar-jogo/visualizar-jogo.component';
 
 const routes: Routes = [
-  { path: '**', redirectTo: 'lista-jogos', pathMatch: 'full' },
+  { path: '', redirectTo: 'lista-jogos', pathMatch: 'full' },
   { path: 'lista-jogos', component: ListaJogosComponent },
-  { path: 'novo-jogo', component: NovoJogoComponent }
+  { path: 'novo-jogo', component: NovoJogoComponent },
+  { path: 'visualizar-jogo/:id', component: VisualizarJogoComponent }
 ];
 
 @NgModule({
