3dd59e7b1c1eae576947276703a6aeb72d6fcdd6
Expedito <expeditopaivaneto@gmail.com>
2024-09-17 23:49:31 -0300

    adicionando bibliotecas no model de jogos

--- src/app/pages/jogos/jogos.module.ts
+++ src/app/pages/jogos/jogos.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { JogosRoutingModule } from './jogos-routing.module';
 
 //Pages
 import { ListaJogosComponent } from './lista-jogos/lista-jogos.component';
 import { NovoJogoComponent } from './novo-jogo/novo-jogo.component';
+import { VisualizarJogoComponent } from './visualizar-jogo/visualizar-jogo.component';
 
 
 @NgModule({
   declarations: [
     ListaJogosComponent,
-    NovoJogoComponent
+    NovoJogoComponent,
+    VisualizarJogoComponent
   ],
   imports: [
     CommonModule,
-    JogosRoutingModule
+    JogosRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi())
   ]
 })
 export class JogosModule { }
