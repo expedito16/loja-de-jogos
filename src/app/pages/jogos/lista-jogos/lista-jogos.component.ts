bf3231a1fc03a37f09e9b522ac88252455a40556
Expedito <expeditopaivaneto@gmail.com>
2024-09-17 22:46:19 -0300

    Estilizando pagina, realizando integracao, integrando tabela e criando mensagem quando nao existir lista de jogos

--- src/app/pages/jogos/lista-jogos/lista-jogos.component.ts
+++ src/app/pages/jogos/lista-jogos/lista-jogos.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JogosService } from '../jogos.service';
 
 @Component({
   selector: 'app-lista-jogos',
   templateUrl: './lista-jogos.component.html',
   styleUrl: './lista-jogos.component.scss'
 })
-export class ListaJogosComponent {
+export class ListaJogosComponent implements OnInit {
 
+  listaJogos: any[] = [];
+
+  constructor(
+    private router: Router,
+    private service: JogosService
+  ) {}
+
+  ngOnInit() {
+    this.getlistaJogos();
+  }
+
+  novoJogo(): void {
+    this.router.navigate(['novo-jogo']);
+  }
+
+  getlistaJogos(): void {
+    this.service.getListaJogos().subscribe((dados) => {
+      this.listaJogos = dados;
+    })
+  }
 }
