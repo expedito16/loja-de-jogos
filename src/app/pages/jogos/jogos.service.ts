ccbc3167550921f60edf6bf57ef1590873ea6e42
Expedito <expeditopaivaneto@gmail.com>
2024-09-17 22:33:15 -0300

    Criando service e endpoint de lista de jogos e criar um novo jogo

---
+++ src/app/pages/jogos/jogos.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JogosService {
+  url = 'http://localhost:3000/listaJogos';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getListaJogos(): Observable<any> {
+    return this.http.get(this.url)
+  }
+
+  criarNovoJogo(jogo: any): Observable<any> {
+    return this.http.post(this.url, jogo);
+  }
+}
